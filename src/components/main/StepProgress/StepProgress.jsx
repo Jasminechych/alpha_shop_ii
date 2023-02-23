9c3d9d3690e7de34fa7fb1164d31625db03ab486
jasminechych <jasminechych@gmail.com>
2023-02-23 22:06:14 +0800

    Restructure StepProgress component.

--- src/components/main/StepProgress/StepProgress.jsx
+++ src/components/main/StepProgress/StepProgress.jsx
@@ -1,160 +1,50 @@
-import { useState, useEffect } from "react";
 import { ReactComponent as PgComplete } from "src/assets/icons/pg-complete.svg";
 
 import style from "src/components/Main/StepProgress/StepProgress.module.scss";
 
-// 第一種
 function StepProgress({ step }) {
   return (
     <div className={style.stepProgressContainer}>
-      <div className={step === 1 ? style.step1 : null}>
-        {step > 1 ? <PgComplete /> : 1}
-      </div>
-      <span
-        className={step >= 1 ? style.activeConnector : style.stepConnecter}
-      ></span>
-      <div
-        className={step > 2 ? null : step === 2 ? style.activeStep : style.step}
-      >
-        {step > 2 ? <PgComplete /> : 2}
-      </div>
-      <span
-        className={step >= 2 ? style.activeConnector : style.stepConnecter}
-      ></span>
-      <div className={step === 3 ? style.activeStep : style.step}>3</div>
+      <StepItem
+        stepNumber="1"
+        isStep1Active={step === 1}
+        isStepComplete={step > 1}
+      />
+      <StepConnecter isConnecterActive={step > 0} />
+      <StepItem
+        stepNumber="2"
+        isStepActive={step === 2}
+        isStepComplete={step > 2}
+      />
+      <StepConnecter isConnecterActive={step > 1} />
+      <StepItem
+        stepNumber="3"
+        isStepActive={step === 3}
+        isStepComplete={step > 3}
+      />
     </div>
   );
 }
 
+function StepItem({ stepNumber, isStep1Active, isStepActive, isStepComplete }) {
+  return (
+    <div
+      className={style.step}
+      data-active={isStepActive}
+      data-oneactive={isStep1Active}
+    >
+      {isStepComplete ? <PgComplete /> : stepNumber}
+    </div>
+  );
+}
+
+function StepConnecter({ isConnecterActive }) {
+  return (
+    <span
+      className={style.stepConnecter}
+      data-active={isConnecterActive}
+    ></span>
+  );
+}
+
 export default StepProgress;
-
-
-
-// 第二種
-// 抽出 Step component
-// 以傳入的 step props 判斷 StepProgress 要渲染 Step 圓圈還是 Complete icon
-
-// function StepProgress({ step }) {
-//   return (
-//     <div className={style.stepProgressContainer}>
-//       {step > 1 ? (
-//         <PgComplete />
-//       ) : (
-//         <Step num="1" className={step === 1 ? style.step1 : null} />
-//       )}
-//       <span
-//         className={step >= 1 ? style.activeConnector : style.stepConnecter}
-//       ></span>
-//       {step > 2 ? (
-//         <PgComplete />
-//       ) : (
-//         <Step
-//           num="2"
-//           className={
-//             step > 2 ? null : step === 2 ? style.activeStep : style.step
-//           }
-//         />
-//       )}
-
-//       <span
-//         className={step >= 2 ? style.activeConnector : style.stepConnecter}
-//       ></span>
-//       {step > 3 ? (
-//         <PgComplete />
-//       ) : (
-//         <Step num="3" className={step === 3 ? style.activeStep : style.step} />
-//       )}
-//     </div>
-//   );
-// }
-
-// function Step({ num, className }) {
-//   return <div className={className}>{num}</div>;
-// }
-
-
-
-
-// 第三種
-// 把每個 step 會有的狀態列出來
-// 當 useEffect 的 dependency step 改變時做狀態切換
-
-// function StepProgress({ step }) {
-//   const [action, setAction] = useState({
-//     isStep3Active: false,
-//     isStep3ConnectorActive: false,
-//     isStep2Complete: false,
-//     isStep2Active: false,
-//     isStep2ConnectorActive: false,
-//     isStep1Complete: false,
-//     isStep1Active: false,
-//   });
-
-//   useEffect(() => {
-//     switch (step) {
-//       case 1:
-//         setAction({
-//           isStep3Active: false,
-//           isStep3ConnectorActive: false,
-//           isStep2Complete: false,
-//           isStep2Active: false,
-//           isStep2ConnectorActive: true,
-//           isStep1Complete: false,
-//           isStep1Active: true,
-//         });
-//         break;
-//       case 2:
-//         setAction({
-//           isStep3Active: false,
-//           isStep3ConnectorActive: true,
-//           isStep2Complete: false,
-//           isStep2Active: true,
-//           isStep2ConnectorActive: true,
-//           isStep1Complete: true,
-//           isStep1Active: false,
-//         });
-//         break;
-//       case 3:
-//         setAction({
-//           isStep3Active: true,
-//           isStep3ConnectorActive: true,
-//           isStep2Complete: true,
-//           isStep2Active: false,
-//           isStep2ConnectorActive: true,
-//           isStep1Complete: true,
-//           isStep1Active: false,
-//         });
-//         break;
-//       default:
-//         throw new Error("Unknown step");
-//     }
-//   }, [step]);
-
-//   return (
-//     <div className={style.stepProgressContainer}>
-//       <div className={action.isStep1Active ? style.step1 : null}>
-//         {action.isStep1Complete ? <PgComplete /> : 1}
-//       </div>
-//       <span
-//         className={
-//           action.isStep2ConnectorActive
-//             ? style.activeConnector
-//             : style.stepConnecter
-//         }
-//       ></span>
-//       <div className={action.isStep2Active ? style.activeStep : style.step}>
-//         {action.isStep2Complete ? <PgComplete /> : 2}
-//       </div>
-//       <span
-//         className={
-//           action.isStep3ConnectorActive
-//             ? style.activeConnector
-//             : style.stepConnecter
-//         }
-//       ></span>
-//       <div className={action.isStep3Active ? style.activeStep : style.step}>
-//         3
-//       </div>
-//     </div>
-//   );
-// }
